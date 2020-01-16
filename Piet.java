abb801da0d9ad743ed847cd1efade31de7e2a296
Zhijun Sheng <zhijunsheng@gmail.com>
2020-01-15 19:24:25 -0500

    added a JPanel

--- Piet.java
+++ Piet.java
@@ -1,14 +1,25 @@
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.Graphics;
+
+class CanvasPanel extends JPanel {
+  @Override
+  public void paintComponent(Graphics g) {
+    g.drawLine(0, 0, getWidth(), getHeight());
+  }
+}
 
 class Piet {
-
-  
-  public static final void main(String[] args) {
+  Piet() {
     JFrame f = new JFrame("Piet Mondrian");
     f.setSize(500, 309);
     f.setLocation(50, 50);
+    f.add(new CanvasPanel());
     f.setVisible(true);
-    System.out.println("Hello, Piet!");
+  }
+  
+  public static final void main(String[] args) {
+    new Piet();
   }
 }
 
