86959acb737584e96a4046b29412e8bc57cfa893
Zhijun Sheng <zhijunsheng@gmail.com>
2020-01-15 19:14:43 -0500

    created a JFrame

---
+++ Piet.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+class Piet {
+
+  
+  public static final void main(String[] args) {
+    JFrame f = new JFrame("Piet Mondrian");
+    f.setSize(500, 309);
+    f.setLocation(50, 50);
+    f.setVisible(true);
+    System.out.println("Hello, Piet!");
+  }
+}
+
